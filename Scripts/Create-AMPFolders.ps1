47979b57ed692d9fc87259368809c69171456a1d
Unknown <adamalbers@gmail.com>
2016-09-28 15:44:27 -0400

    Updated AMP Folders
    Change from hard coded C: to $Env:SystemDrive for the odd client who isn't using C: for the OS drive.

--- Scripts/Create-AMPFolders.ps1
+++ Scripts/Create-AMPFolders.ps1
@@ -1,11 +1,11 @@
 # Create standard AMP directories for holding logs, scripts, etc.
 # The passwordsPath should only be used to store secure string passwords. If you have plaintext passwords in here, you're doing it wrong.
  
- $rootPath = "C:\AMP"
- $logPath = "C:\AMP\Logs"
- $passwordPath = "C:\AMP\Passwords"
- $scriptPath = "C:\AMP\Scripts"
- $reportPath = "C:\AMP\Reports"
+ $rootPath = "$Env:SystemDrive/AMP"
+ $logPath = "$Env:SystemDrive/AMP/Logs"
+ $passwordPath = "$Env:SystemDrive/AMP/Passwords"
+ $scriptPath = "$Env:SystemDrive/AMP/Scripts"
+ $reportPath = "$Env:SystemDrive/AMP/Reports"
   
   $pathArray = $rootPath,$logPath,$passwordPath,$scriptPath,$reportPath
    
