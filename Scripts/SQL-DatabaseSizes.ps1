5892e40b25f507309d605c82ac3f3d6f482ef26b
Adam Albers <adamalbers@gmail.com>
2016-09-28 20:51:26 -0400

    Updated event log writes.

--- Scripts/SQL-DatabaseSizes.ps1
+++ Scripts/SQL-DatabaseSizes.ps1
@@ -1,8 +1,29 @@
-# Get sizes of each individual database for all SQL instances on the computer
+<#
+.SYNOPSIS
+	Get sizes of each database for all SQL instances on localhost
+.NOTES
+	Author: Adam Albers
+#>
+# Event log settings to use for custom events
+# You must have already created your custom event log and sources.
+# That is outside the scope of this script.
+$logName = "AMPSystems"
+$eventSource = "AMP SQL Report"
+$eventIDInfo = "1100"
+$eventIDError = "1101"
+$entryType = "Information"
 
+# Report file path
+$reportPath = "$Env:SystemDrive/AMP/Reports/SQLDatabases-$(Get-Date -f yyyy-MM-dd).txt"
+
+# Load SQL assembly
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.Smo') | Out-Null
 
+# Create array to hold database objects
 $myDatabases = @()
+
+# Create variable to hold event messages
+$message = ""
 
 ForEach ($Instance in (Get-WmiObject -Class Win32_Service -ComputerName $Env:ComputerName | Where {$_.Name -like 'MSSQL$*'}))
 {
@@ -19,13 +40,20 @@ ForEach ($Instance in (Get-WmiObject -Class Win32_Service -ComputerName $Env:Com
         	$currentDatabase.Database = $Database.Name
         	$currentDatabase.DatabaseSizeGB = [Math]::Round($Database.Size/1024,2)
         	
-		#Check for SQL Express Limitations
+		# Check for SQL Express Limitations
 		# List SizeLimit as % of limit (4GB per database for SQL Express 2008 and earlier,
 		# 10GB for 2008R2 and later)
 		If ($Sql.Edition -like "*Express*")
 		{
-			If ($Sql.Version -lt "10.5") {$currentDatabase.SizeLimit = "{0:P}" -f ($currentDatabase.DatabaseSizeGB / 4)}
-			Else {$currentDatabase.SizeLimit = "{0:P}" -f ($currentDatabase.DatabaseSizeGB / 10)}
+			If ($Sql.Version -lt 10.5) {$currentDatabase.SizeLimit = "{0:P}" -f ($currentDatabase.DatabaseSizeGB / 4)}
+			Else {$currentDatabase.SizeLimit = "{0:P2}" -f ($currentDatabase.DatabaseSizeGB / 10)}
+			
+			If ($currentDatabase.SizeLimit -gt 80)
+			{
+			$entryType = "Error"
+			$message = "$($currentDatabase.Database | Out-String) is at $($currentDatabase.SizeLimit | Out-String) of maximum size"
+			Write-EventLog -LogName $logName -Source $eventSource -EventID $eventIDError -EntryType $entryType -Message $message
+			}
 		}
 
 		$myDatabases += $currentDatabase
@@ -35,5 +63,6 @@ ForEach ($Instance in (Get-WmiObject -Class Win32_Service -ComputerName $Env:Com
         Clear-Variable Sql -ErrorAction SilentlyContinue
         Clear-Variable InstanceString -ErrorAction SilentlyContinue
 }
-
-$myDatabases | Out-File $Env:SystemDrive/AMP/Reports/SQLdatabases-$(Get-Date -f yyyy-MM-dd).txt
+$myDatabases | Out-File $reportPath
+$message = Get-Content $reportPath | Format-Table -AutoSize | Out-String
+Write-EventLog -LogName $logName -Source $eventSource -EventID $eventIDInfo -EntryType "Information" -Message $message
