a9cc52674517fa31ce5f0badcf7568eded1b4f9b
Unknown <adamalbers@gmail.com>
2016-09-29 13:52:49 -0400

    Added $reportPath to output.

--- Scripts/Get-Logons.ps1
+++ Scripts/Get-Logons.ps1
@@ -11,4 +11,4 @@ $reportPath = "$Env:SystemDrive/AMP/Reports/UserLogons-$(Get-Date -f yyyy-MM-dd)
 # Create column headings
 $tableFormat = @{Expression={$_.TimeCreated};Label="Logon Time"},@{Expression={$_.Properties[5].Value};Label="User"},@{Expression={$_.Properties[8].Value};Label="Logon Type"},@{Expression={$_.Properties[18].Value};Label="Source IP Address"}
 
-Get-WinEvent -FilterHashTable @{LogName='Security'; id=4624} | Where-Object {($_.Properties[8].Value -eq 2) -or ($_.Properties[8].Value -eq 10)} | Format-Table $tableFormat -AutoSize -Wrap
+Get-WinEvent -FilterHashTable @{LogName='Security'; id=4624} | Where-Object {($_.Properties[8].Value -eq 2) -or ($_.Properties[8].Value -eq 10)} | Format-Table $tableFormat -AutoSize -Wrap | Out-File $reportPath
