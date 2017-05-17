fbfe36e5109655c2bbd42092009bb8b351d36c3c
Adam Albers <adam@ampsysllc.com>
2017-05-17 10:50:23 -0400

    Connect to O365 Compliance Center script

---
+++ Scripts/Connect-Office365SCC.ps1
@@ -0,0 +1,5 @@
+# Get login credentials 
+$UserCredential = Get-Credential 
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid -Credential $UserCredential -Authentication Basic -AllowRedirection 
+Import-PSSession $Session -AllowClobber -DisableNameChecking 
+$Host.UI.RawUI.WindowTitle = $UserCredential.UserName + " (Office 365 Security & Compliance Center)" 
