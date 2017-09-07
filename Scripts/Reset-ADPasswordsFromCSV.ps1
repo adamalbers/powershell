905942abc22342518f35b4f3f6b5ce2a6454eb45
Adam Albers <adamalbers@gmail.com>
2017-09-07 11:18:58 -0400

    Added Reset-ADPasswordFromCSV.ps1

---
+++ Scripts/Reset-ADPasswordsFromCSV.ps1
@@ -0,0 +1,18 @@
+Import-Module ActiveDirectory
+
+$csvPath = "$Env:SystemDrive/AMP/Scripts/ADPasswordReset.csv"
+# Import users and passwords from CSV
+Import-Csv $csvPath | ForEach-Object {
+$samAccountName = $_."samAccountName"
+$newPassword = $_."password"
+  
+# Reset user password.
+Set-ADAccountPassword -Identity $samAccountName -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $newPassword -Force)
+ 
+# Force user to reset password at next logon.
+# Remove this line if not needed for you
+Set-AdUser -Identity $samAccountName -ChangePasswordAtLogon $true
+
+# Output each user being reset
+Write-Host "AD Password has been reset for: "$samAccountName
+}
