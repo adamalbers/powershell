95e28d26c3a1787788563190c8dcdd65acca335d
Adam Albers <adamalbers@gmail.com>
2017-12-02 07:11:41 -0500

    1) Fixed case on variables. 2) Changed to Office365 SMTP. 3) Removed logFile attachment as it was frequently too large to send and would prevent email from being sent.

--- Scripts/Robocopy-EmailNotification.ps1
+++ Scripts/Robocopy-EmailNotification.ps1
@@ -1,31 +1,31 @@
 # Requires PowerShell 3.0 or higher.
 # This is intended to be used for on-demand robocopy jobs. It will not save the email password so this won't work as a scheduled task.
 
-  $SourceFolder = "c:\example\path\source"
-  $DestinationFolder = "\\example\path\destination"
-  $FileList = "*.txt"
-  $Logfile = "$Env:SystemDrive/AMP/Logs/Robocopy-" + (Get-Date -uFormat "%m%d%Y%H%M%S").tostring() + ".log"
+$sourceFolder = "c:\example\path\source"
+$destinationFolder = "\\example\path\destination"
+$fileList = "*.*"
+$logFile = "C:\AMP\Logs\Robocopy-" + (Get-Date -uFormat "%m%d%Y%H%M%S").tostring() + ".log"
+  
 
-    $EmailFrom = "ampsys@ampsyscloud.com"
 # Prompt for email password when running script
-    $credentials = Get-Credential -UserName $EmailFrom -Message "Email Password"
-
+$credentials = Get-Credential -UserName "Office365 Email Address" -Message "Email Password"
+  
 # Mutliple recipients in the form @("first@person.com","second@person.com")
-      $EmailTo = @("adam@ampsysllc.com")
-
-        $EmailBody = "Robocopy - See attached log file for details"
-        $EmailSubject = "Robocopy Done on $env:COMPUTERNAME"
-        $SMTPServer = "smtp.sendgrid.net"
-        $SMTPPort = "587"
-
+$emailTo = @("alerts@ampsysllc.com")
+$emailFrom = $credentials.UserName  
+$emailBody = "Robocopy - See log file for details: $logFile"
+$emailSubject = "Robocopy Done on $env:COMPUTERNAME"
+$smtpServer = "smtp.office365.com"
+$smtpPort = "587"
+  
 # Load Robocopy Arguments
 # See https://technet.microsoft.com/en-us/library/cc733145.aspx for options
 # If you use /COPYALL you must Run As Administrator
-          $Options = @("/R:0","/W:0","/Z","/LOG+:$Logfile","/TEE","/S","/E")
-          $RobocopyArgs = @($SourceFolder,$DestinationFolder,$FileList,$Options)
-
+$options = @("/R:0","/W:0","/ZB","/LOG+:$Logfile","/TEE","/S","/E")
+$robocopyArgs = @($SourceFolder,$DestinationFolder,$FileList,$Options)
+  
 # Run robocopy
-            robocopy @RobocopyArgs
-
+robocopy @robocopyArgs
+  
 # Send email with log attached
-              Send-MailMessage -From $EmailFrom -To $EmailTo -Subject $EmailSubject -Attachments $Logfile -Body $EmailBody -SmtpServer $SMTPServer -Port $SMTPPort -Credential $credentials -UseSSL
+Send-MailMessage -From $emailFrom -To $emailTo -Subject $emailSubject -Body $emailBody -SmtpServer $smtpServer -Port $smtpPort -Credential $credentials -UseSSL
