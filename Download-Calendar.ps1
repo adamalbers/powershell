d6b50493bf8c26470682361f417d65c412d7278b
Adam Albers <adamalbers@protonmail.com>
2019-01-14 21:24:59 +0000

    Cleaned up formatting and improved header.

--- Download-Calendar.ps1
+++ Download-Calendar.ps1
@@ -1,6 +1,9 @@
-<#
-        .SYNOPSIS
+<#		
+		.SYNOPSIS
             Downloads monthly calendar sheets from WinCalendar.net and merges them into one workbook for the year.
+			
+		.DESCRIPTION
+			REQUIRES a legend.xlsx in the same folder as script. This legend is appended to the bottom of each month of the calendar.
 
         .NOTES
             Created By Adam Albers
@@ -11,7 +14,7 @@
             The year in yyyy format. Defaults to current year.
 #>
 
-param([string]$year = (Get-Date -Format yyyy)
+param([string]$year = (Get-Date -Format yyyy))
 
 ### Download monthly calendar spreadsheets ###
 
@@ -23,65 +26,65 @@ $path = Get-Location | Select-Object -ExpandProperty Path
 New-Item -Type Directory -Path "$path\\$year" -Force
 
 foreach ($month in $months)
-	{
-		$thisMonth = [string]$month
-		$uri = "$($server)$($thisMonth)-$($year)-Calendar-with-Holidays.xlsx"
-		$index = "{0:00}" -f $([array]::indexOf($months,$month))
-		$filename = "$path\$year\$index-$month-$year-Calendar.xlsx"
-		Write-Host "$filename"
-		$client.DownloadFile($uri,$filename)
-	}
+{
+  $thisMonth = [string]$month
+  $uri = "$($server)$($thisMonth)-$($year)-Calendar-with-Holidays.xlsx"
+  $index = "{0:00}" -f $([array]::IndexOf($months,$month))
+  $filename = "$path\$year\$index-$month-$year-Calendar.xlsx"
+  Write-Host "$filename"
+  $client.DownloadFile($uri,$filename)
+}
 
 # Path for a spreadsheet with the legend to be copied into each worksheet
 $legendPath = "$path\legend.xlsx"
 
 ### Merge the individual spreadsheets into one workbook ###
 
-$excelObject=New-Object -ComObject excel.application
-$excelObject.visible=$false
+$excelObject = New-Object -ComObject excel.application
+$excelObject.visible = $false
 
-$excelFiles=Get-ChildItem -Path "$path\\$year\\*.xlsx"
+$excelFiles = Get-ChildItem -Path "$path\\$year\\*.xlsx"
 
-$workbook=$ExcelObject.Workbooks.add()
-$worksheet=$Workbook.Sheets.Item("Sheet1")
+$workbook = $ExcelObject.Workbooks.Add()
+$worksheet = $Workbook.Sheets.Item("Sheet1")
 
-$legendWorkbook=$ExcelObject.Workbooks.Open($legendPath)
-$legendWorksheet=$LegendWorkbook.Sheets.Item("Legend")
-$legendRange=$LegendWorksheet.Range("A1","I10")
+$legendWorkbook = $ExcelObject.Workbooks.Open($legendPath)
+$legendWorksheet = $LegendWorkbook.Sheets.Item("Legend")
+$legendRange = $LegendWorksheet.Range("A1","I10")
 
 $searchText = "*Calendar*"
 
-foreach($ExcelFile in $ExcelFiles){
- 
-$everyExcel=$excelObject.Workbooks.Open($excelFile.FullName)
-$everySheet=$everyExcel.sheets.item(1)
-$range=$everySheet.Range("O1","O1")
-$range.clear()
-$range=$everySheet.Range("B2","B2")
-$range.clear()
+foreach ($ExcelFile in $ExcelFiles) {
 
-	for ($i = $everySheet.usedrange.rows.count; $i -gt 33; $i--)
-	{
-		$foundText = $everySheet.Range("A:Z").Find($searchText)
-		If ($foundText) {
-		Write-Host ($foundText.Row)
-		$foundText.EntireRow.Delete()
-		}
-	}
+  $everyExcel = $excelObject.Workbooks.Open($excelFile.FullName)
+  $everySheet = $everyExcel.Sheets.Item(1)
+  $range = $everySheet.Range("O1","O1")
+  $range.Clear()
+  $range = $everySheet.Range("B2","B2")
+  $range.Clear()
 
-$legendRange.Copy() | Out-Null
-$lastRow = $Everysheet.UsedRange.Rows.Count + 2
-$everySheet.Range("C"+$lastrow).Activate()
-$everySheet.Paste()
+  for ($i = $everySheet.usedrange.rows.count; $i -gt 33; $i --)
+  {
+    $foundText = $everySheet.Range("A:Z").Find($searchText)
+    if ($foundText) {
+      Write-Host ($foundText.Row)
+      $foundText.EntireRow.Delete()
+    }
+  }
+
+  $legendRange.Copy() | Out-Null
+  $lastRow = $Everysheet.usedrange.rows.count + 2
+  $everySheet.Range("C" + $lastrow).Activate()
+  $everySheet.Paste()
 
 
 
-$everySheet.Copy($Worksheet)
-$everyExcel.Close($false)
- 
+  $everySheet.Copy($Worksheet)
+  $everyExcel.Close($false)
+
 }
 
-$workbook.Worksheets.item("Sheet1").Delete()
+$workbook.Worksheets.Item("Sheet1").Delete()
 
 
 
