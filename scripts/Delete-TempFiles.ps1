b075a529a6bba85680edd48920ae93d060e86981
Adam Albers <adamalbers@protonmail.com>
2019-12-23 16:07:09 -0500

    Improved the temp files cleanup script and added SyncroMSP integration.

--- scripts/Delete-TempFiles.ps1
+++ scripts/Delete-TempFiles.ps1
@@ -1,2 +1,55 @@
-​​​​​​​Remove-Item "$env:windir/Temp/*" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:systemdrive/Users/*/AppData/Local/Temp/*" -Recurse -Force -ErrorAction SilentlyContinue
+Import-Module $env:SyncroModule
+
+# Enter your Company information here:
+$subdomain = "example"
+
+$ticketDuration = 5  # This is the number of minutes you want added to the ticket
+
+$daysToDelete = 7  # Enter the number of days of log files to keep here
+$ErrorActionPreference = "SilentlyContinue"
+
+$sizeBefore = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName, @{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } }, @{ Name = "Size (GB)" ; Expression = {"{0:N1}" -f( $_.Size / 1gb)}}, @{ Name = "FreeSpace (GB)" ; Expression = {"{0:N1}" -f( $_.Freespace / 1gb ) } }, @{ Name = "PercentFree" ; Expression = {"{0:P1}" -f( $_.FreeSpace / $_.Size ) } } | Format-Table -AutoSize | Out-String                      
+                    
+# Stops the windows update service. 
+Get-Service -Name wuauserv | Stop-Service -Force -ErrorAction SilentlyContinue
+
+# Deletes the contents of windows software distribution.
+Get-ChildItem "$Env:SystemDrive\Windows\SoftwareDistribution\*" -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { ($_.LastWriteTime -lt $(Get-Date).AddDays(-$daysToDelete)) } | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue    
+
+# Deletes the contents of the Windows Temp folder.
+Get-ChildItem "$Env:Systemdrive\Windows\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { ($_.LastWriteTime -lt $(Get-Date).AddDays(-$daysToDelete)) } | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+             
+# Delets all files and folders in user's Temp folder. 
+Get-ChildItem "$Env:Systemdrive\Users\*\AppData\Local\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { ($_.LastWriteTime -lt $(Get-Date).AddDays(-$daysToDelete))} | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+
+# Remove all files and folders in user's Temporary Internet Files. 
+Get-ChildItem "$Env:Systemdrive\Users\*\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {($_.LastWriteTime -le $(Get-Date).AddDays(-$daysToDelete))} | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+                    
+# Cleans IIS Logs if applicable.
+Get-ChildItem "$Env:Systemdrive\inetpub\logs\LogFiles\*" -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { ($_.LastWriteTime -le $(Get-Date).AddDays(-$daysToDelete)) } | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+
+$sizeAfter =  Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName, @{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } }, @{ Name = "Size (GB)" ; Expression = {"{0:N1}" -f( $_.Size / 1gb)}}, @{ Name = "FreeSpace (GB)" ; Expression = {"{0:N1}" -f( $_.Freespace / 1gb ) } }, @{ Name = "PercentFree" ; Expression = {"{0:P1}" -f( $_.FreeSpace / $_.Size ) } } | Format-Table -AutoSize | Out-String
+
+# Restart the Windows Update Service
+Get-Service -Name wuauserv | Start-Service
+
+
+# Create Ticket and get the ticket number
+$syncroTicket = Create-Syncro-Ticket -Subdomain "$subdomain" -Subject "Cleanup for $Env:ComputerName" -IssueType "Other" -Status "New"
+$ticket = $syncroTicket.ticket.number
+
+$body = "Cleaned temporary files. `
+            Free space before: $sizeBefore `
+            Free space after: $sizeAfter"
+
+# Add time to ticket
+$startAt = (Get-Date).AddMinutes(-10).toString("o")
+Create-Syncro-Ticket-TimerEntry -Subdomain "$subdomain" -TicketIdOrNumber $ticket -StartTime $startAt -DurationMinutes $ticketDuration -Notes "Cleanup of temporary files."
+
+# Add ticket notes
+Create-Syncro-Ticket-Comment -Subdomain "$subdomain" -TicketIdOrNumber $ticket -Subject "Temp File Cleanup for $Env:ComputerName" -Body "$body" -Hidden $False -DoNotEmail $True
+
+#Close Ticket
+Update-Syncro-Ticket -Subdomain "$subdomain" -TicketIdOrNumber $ticket -Status "Resolved"
+
+Exit 0
---
+++ scripts/Register-ChocolateyPackages.ps1
@@ -0,0 +1,58 @@
+# This script will register Chocolatey packages in Programs and Features.
+# This exposes the packages to inventory by RMMs etc.
+
+$companyUrl = "http://www.example.com"
+$companyName = "Example Inc."
+
+# Set Chocolatey path
+$chocoPath = "$Env:SystemDrive\ProgramData\Chocolatey\Bin\Choco.exe"
+
+# Path to file that will hold list of installed Chocolatey packages
+$chocoListPath = "$Env:Temp\chocoList.txt"
+
+# Set program icon for Programs and Features list
+# Will use the Chocolatey icon so it's easy to see which items were installed via Chocolatey
+$icon = $chocoPath
+
+Function createPackage ($package) {
+    
+    $packageName = $package.Split()[0]
+    $packageVersion = $package.Split()[1]
+    
+    $registryPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Chocolatey - $packageName"
+
+    if (!(Test-Path $registryPath)) {
+        # Create the registry key
+        New-Item $registryPath -Force | Out-Null
+    }
+    
+    Write-Host "Adding $package to Programs and Features"   
+
+    # Create key properties
+    New-ItemProperty -Path $registryPath -Name "DisplayName" -PropertyType String -Value "$packageName" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "DisplayVersion" -PropertyType String -Value "$packageVersion" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "Publisher" -PropertyType String -Value "Deployed by $companyName" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "UrlInfoAbout" -PropertyType String -Value "$companyUrl" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "Comments" -PropertyType String -Value "Chocolatey package installed by $companyName" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "DisplayIcon" -PropertyType String -Value "$icon" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "UninstallString" -PropertyType String -Value "$chocoPath uninstall $packageName -force -yes" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "QuietUninstallString" -PropertyType String -Value "$chocoPath uninstall $packageName -force -yes" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "NoModify" -PropertyType DWORD -Value "1" | Out-Null
+    New-ItemProperty -Path $registryPath -Name "NoRepair" -PropertyType DWORD -Value "1" | Out-Null
+}
+
+# TODO: Delete entries for packages no longer installed
+
+# Create list of currently installed packages
+Start-Process $chocoPath -ArgumentList "list -localonly" -NoNewWindow -RedirectStandardOutput "$chocoListPath"
+Start-Sleep -Seconds 2
+$chocolateyPackageList = [System.Collections.ArrayList](Get-Content "$chocoListPath")
+
+# Remove the lines of the list that are not actual package names
+ForEach ($package in $chocolateyPackageList) {
+   if (!($package -like "*chocolatey*" -or $package -like "*packages installed*" -or $package -like "*automatically sync*" -or $package -like "*package synchronizer*")) {
+         createPackage("$package")
+    }
+}
+
+Exit 0
