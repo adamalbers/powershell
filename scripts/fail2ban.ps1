1e31b0f03a9e87f6c4f68d27f06291d639927ef3
Adam Albers <adamalbers@protonmail.com>
2020-04-23 20:38:41 -0400

    Added basic, pseudo-fail2ban script.

---
+++ scripts/fail2ban.ps1
@@ -0,0 +1,26 @@
+$ErrorActionPreference = "SilentlyContinue"
+
+# Search Security event logs for IPs with more than 4 failed login events in the past 10 min.
+$attackerIPs = Get-EventLog -LogName "Security" -InstanceId 4625 -After (Get-Date).AddMinutes(-10) | Select-Object @{Label="SourceIP";Expression={$_.ReplacementStrings[-2]}} | Group-Object -Property SourceIP | Where-Object {$_.Count -gt 4} | Sort-Object -Property Count -Descending
+
+if ($attackerIPs -eq $null) {
+    Write-Output "No IPs to ban. Exiting."
+    Set-NetFirewallRule -DisplayName "fail2ban" -Enabled:$false
+    
+    Exit 0    
+}
+
+Write-Output $attackerIPs
+
+# Add $attackerIPs to the fail2ban firewall rule. This replaces all existing IPs in the rule and thus unbans IPs after the 10 minute Window.
+if (Get-NetFirewallRule -DisplayName "fail2ban") {
+    Set-NetFirewallRule -DisplayName "fail2ban" -RemoteAddress $($attackerIPs.Name) -Enabled:$true
+    
+	Exit 0
+}
+else {
+	New-NetFirewallRule -DisplayName "fail2ban" -RemoteAddress $($attackerIPs.Name) -Direction Inbound -Action Block -Protocol Any
+}
+
+
+Exit 0
