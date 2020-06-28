04f1b9708b1b382d135bf6b4eefc2d2da4bddbab
Adam Albers <adamalbers@protonmail.com>
2020-06-28 08:55:44 -0400

    Updated default options for Robocopy.

--- scripts/Robocopy-EmailNotification.ps1
+++ scripts/Robocopy-EmailNotification.ps1
@@ -21,7 +21,7 @@ $smtpPort = "587"
 # Load Robocopy Arguments
 # See https://technet.microsoft.com/en-us/library/cc733145.aspx for options
 # If you use /COPYALL you must Run As Administrator
-$options = @("/R:0","/W:0","/ZB","/LOG+:$Logfile","/TEE","/COPYALL","/E")
+$options = @("/R:0","/W:0","/ZB","/LOG+:$Logfile","/TEE","/COPYALL","/E","/XO","/SL","/MT")
 $robocopyArgs = @($SourceFolder,$DestinationFolder,$FileList,$Options)
   
 # Run robocopy
