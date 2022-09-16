61e425e4e62685eb34613ef524ebc17839b83b7f
Adam Albers <adamalbers@gmail.com>
2022-09-16 17:56:54 -0400

    Made profile.ps1 output a little prettier.

--- profile.ps1
+++ profile.ps1
@@ -5,6 +5,7 @@ $configPath = "/path/to/powershell/configs"
 
 # ----- DO NOT MODIFY BELOW THIS LINE ----- #
 Clear-Host
+Write-Host '#----- Loading Profile -----#'
 Write-Host "PowerShell Version: $($PSVersionTable.PSVersion.ToString())"
 Write-Host '------------------------------'
 Write-Host -ForegroundColor Green "Using $PROFILE"
@@ -14,17 +15,18 @@ Write-Host -ForegroundColor Green "Configs path: $configsPath"
 Write-Host '------------------------------'
 Write-Host 'Importing $coreFunctions from profile:'
 
-# Source checkUnique and importFunction (requires checkUnique).
-. "$($functionsPath)/checkUnique.ps1"
-. "$($functionsPath)/importFunction.ps1"
-
 # Define any other core functions we want.
-$coreFunctions = @('importAllFunctions.ps1',
+# Both importFunction and importConfig require checkUnique.
+$coreFunctions = @('checkUnique.ps1',
+                    'importFunction.ps1',
                     'importConfig.ps1'
                 )
 
 # Loop through $coreFunctions and source them with importFunction
 $coreFunctions | ForEach-Object {
     Write-Host -ForegroundColor Green "Importing $_" 
-    importFunction $_
+    . "$($functionsPath)/$($_)"
 }
+
+Write-Host '------------------------------'
+Write-Host '#----- Profile Loaded -----#'
